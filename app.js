c36779e0c2d61652afcb62552ac80b8090e3a65b
Chris Holman <squid@sirsquidness.com>
2016-03-08 23:37:03 +1100

    Refactor significantly for greater good

--- app.js
+++ app.js
@@ -5,10 +5,10 @@
 var listener = require("./listener.js");
 
 var l = listener.create({port: 27036, ip: "0.0.0.0"} );
-
 l.on("client_seen", function(d) { console.log("Saw: " + d.steam_id); });
 l.on("raw_message", function(m) {
     var rinfo = m.sender;
     console.log(`got message from ${rinfo.address}:${rinfo.port}`);
 });
+l.on("invalid_packet", function(d) { console.log("Invalid packet, reason: " + d.reason); });
 
--- listener.js
+++ listener.js
@@ -1,5 +1,12 @@
+"use strict";
 
 
+/**
+ * TODO:
+ *    * Add length checking everywhere in teh program to stop invalid packets breaking the app
+ *    * Add a funciton to send a broadcast packet to prompt discovery replies 
+ *    * ???
+ */
 
 var protobuf = require("protocol-buffers"),
     dgram = require("dgram"),
@@ -10,23 +17,42 @@ var protobuf = require("protocol-buffers"),
 
 var messageTypes = protobuf(fs.readFileSync("steamdiscover.proto"));
 
+var packetMagicBytes = new Buffer([0xff, 0xff, 0xff, 0xff, 0x21, 0x4c, 0x5f, 0xa0]);
 
-function create(opts) {
-  opts = opts || {};
+class Listener extends events {
+  constructor(opts) {
+    super();
+    this.opts = opts || {};
+    this.initSocket();
 
-  var event_emitter = new events();
+  }
 
-  var server = dgram.createSocket("udp4");
-  
-  server.on("message", (m, rinfo) => {
-    event_emitter.emit("raw_message", {message: m, sender: rinfo });
+
+  initSocket() {
+    this.server = dgram.createSocket("udp4");
+    this.server.on("message", this.receiveMessage.bind(this));
+    this.server.bind(this.opts.port || 27036, this.opts.ip || "0.0.0.0");
+  }
+
+  // Given a whole packet, read the header, and do stuff!
+  receiveMessage(m, rinfo) {
+    this.emit("raw_message", {message: m, sender: rinfo });
+
+    if (!m || m.length < 20) {
+      this.emit("invalid_packet", {reason: "too short", message: m});
+    }
 
     var offset = 0;
+    var indicator = m.slice(0,8);
+    
+    if (indicator.compare(packetMagicBytes) !== 0) {
+      this.emit("invalid_packet", {reason: "magic bytes mismatch", message: m});
+    }
+    
     offset += 8;
   
     var headerLength = m.readUInt32LE(offset);
     offset += 4; // 32 bit 
-  
   
     var header = m.slice(offset, offset + headerLength);
     var headerContent =  messageTypes.CMsgRemoteClientBroadcastHeader.decode(header);
@@ -38,20 +64,33 @@ function create(opts) {
     
     if (headerContent.msg_type == 1 ) {   /* k)ERemoteClientBroadcastMsgStatus */
       var body_data = m.slice(offset, offset + bodyLength);
-      
-      var body_content = messageTypes.CMsgRemoteClientBroadcastStatus.decode(body_data);
+
+      this.parseBody(body_data, bodyLength);
+
+    }
+  }
+
+  // Given a buffer containing an k_ERemoteClientBroadcastMsgStatus, parse the Steam ID 
+  parseBody(body_buffer, length) {
+
+      var body_content = messageTypes.CMsgRemoteClientBroadcastStatus.decode(body_buffer);
       
       var steamid_buffer = (body_content.users[0] || {} ).steamid;
       if (steamid_buffer) {
         var steamid = bignum.fromBuffer(steamid_buffer, { endian: "little", size: 'auto'} )
-        event_emitter.emit("client_seen", { steam_id: steamid.toString() });
+        this.emit("client_seen", { steam_id: steamid.toString() });
       } 
-    }
-  });
+
+  }
   
-  server.bind(opts.port || 27036, opts.ip || "0.0.0.0");
+}
+
+function create(opts) {
+
+  return new Listener(opts);
+
   
-  return event_emitter;
+  
 }
 
 function broadcastDiscovery(server) {

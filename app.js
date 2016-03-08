da3a1530b9afcec24532c4f4cb0958923e127f3f
Chris Holman <squid@sirsquidness.com>
2016-03-08 22:48:34 +1100

    remove console.logs

--- app.js
+++ app.js
@@ -14,19 +14,15 @@ var messageTypes = protobuf(fs.readFileSync("steamdiscover.proto"));
 var server = dgram.createSocket("udp4");
 
 server.on("message", (m, rinfo) => {
-//try {
   var offset = 0;
-  console.log(`got message: ${m} from ${rinfo.address}:${rinfo.port}`);
-  console.log(m.slice(0,8).toString('hex'));
+  console.log(`got message from ${rinfo.address}:${rinfo.port}`);
   offset += 8;
 
   var headerLength = m.readUInt32LE(offset);
   offset += 4; // 32 bit 
 
-  console.log(`header length: ${headerLength}`);
 
   var header = m.slice(offset, offset + headerLength);
-  console.log(header.toString('hex'));
   var headerContent =  messageTypes.CMsgRemoteClientBroadcastHeader.decode(header);
   console.log(headerContent);
 
@@ -43,17 +39,14 @@ server.on("message", (m, rinfo) => {
     
     var steamid_buffer = (body_content.users[0] || {} ).steamid;
     if (steamid_buffer) {
-//      console.log(steamid_buffer.readUIntLE(0,steamid_buffer.length));
       var steamid = bignum.fromBuffer(steamid_buffer, { endian: "little", size: 'auto'} )
       console.log(steamid.toString());
     } 
   }
-//  } catch (e) { console.log("Error: " + e + "\n\n");}
 });
 
 server.bind(27036);
 
-//messagesTypes["CMsgRemoteClientBroadcastStatus"].decode("");
 
 
 function broadcastDiscovery(server) {

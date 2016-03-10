42571b0c524783d83c47d96c9912ab054cd257e1
Chris Holman <squid@sirsquidness.com>
2016-03-10 21:35:10 +1100

    Now looks up steam username and playing game (see note below)
    The current version of steam-api-node does not return game info.
    You need to include changes from the pull request linked below
    to get game info returned.
    
    https://github.com/DPr00f/steam-api-node/pull/5/files

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 // https://codingrange.com/blog/steam-in-home-streaming-discovery-protocol
 // https://www.npmjs.com/package/protocol-buffers
 // https://github.com/SteamRE/SteamKit/blob/master/Resources/Protobufs/steamclient/steammessages_remoteclient_discovery.proto
+// https://developer.valvesoftware.com/wiki/Steam_Web_API#GetPlayerSummaries_.28v0001.29
 
-var listener = require("./listener.js");
+var listener = require("./listener.js"),
+    steam = require("steam-api"), 
+    fs = require("fs");
+try {
+    var apiKey = fs.readFileSync("./apikey.txt").toString().replace(/[\W \n\r]/g, "");
+} catch (e)
+{ 
+    console.error("Create the file 'apikey.txt' in the working directory, which contains your API key from http://steamcommunity.com/dev/apikey"); 
+    require("process").exit(1);
+}
+
+var steamClient = new steam.User(apiKey);
 
 var l = listener.create({port: 27036, ip: "0.0.0.0"} );
-l.on("client_seen", function(d) { console.log("Saw: " + d.steam_id); });
+l.on("client_seen", function(d) { 
+    steamClient.GetPlayerSummaries(d.steam_id).done(function(playersInfo,r) {
+        playersInfo = Array.isArray(playersInfo) ? playersInfo : [playersInfo];
+        
+        playersInfo.forEach(function(p) {
+            if (p.gameextrainfo) { 
+                var extraInfo = ` playing game '${p.gameextrainfo}'`;
+            }
+            console.log(`Saw steam user ${p.personaName}${extraInfo}`);
+        });
+    });
+});
 l.on("raw_message", function(m) {
     var rinfo = m.sender;
     console.log(`got message from ${rinfo.address}:${rinfo.port}`);

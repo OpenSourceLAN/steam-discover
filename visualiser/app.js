c2dc92db659b53c1f6c0f1f44eb5792dd1cb413a
Chris Holman <squid@sirsquidness.com>
2016-05-01 23:29:42 +1000

    Charting! Only tested with one person, which doesn't really work but lol

--- visualiser/app.js
+++ visualiser/app.js
@@ -1,3 +1,4 @@
+"use strict";
 var redis = require('redis'),
 	socketio = require('socket.io'),
 	http = require('http'),
@@ -30,9 +31,33 @@ server.on("request", (req, res) => {
 
 server.listen(8000);
 
+// This bit will probably scale badly, but it's bed time now and I want to get something working
+var playerState = {};
 var qb = new querybuffer(5000, (update) => {
 	if (update.length == 0)
 		return;
+	var now = new Date();
+
+	// Update teh state of any seen players
+	update.forEach((player) => {
+		player.timestamp = now;
+		playerState[player.steamid] = player;
+	});
+
+	var games = new gameCounter();
+
+	// Purge any old players, aggregate others
+	var expire = new Date(now - 2*60*1000); // 2 minutes
+	Object.keys(playerState).forEach((key) => {
+		var player = playerState[key];
+		if (player.timestamp < expire) {
+			delete playerState[key];
+		} else {
+			games.addToGame(player.gameid, player.gamename);
+		}
+	});
+
+	io.emit("message", games.flatten());
 });
 
 var redisClient = redis.createClient();
@@ -40,3 +65,31 @@ redisClient.on("message", (channel, message) => {
 	qb.addItem(JSON.parse(message));
 });
 redisClient.subscribe("steam-update");
+
+
+class gameCounter {
+	constructor() {
+		this.games = {};
+	}
+
+	addToGame(gameid, gamename) {
+		if (!gameid) {
+			gameid = 0;
+			gamename = "Not in game, or not playing a Steam game";
+		}
+
+		if (this.games[gameid] == undefined) {
+			this.games[gameid] = { id: gameid, hits: 1, name: gamename };
+		} else {
+			this.games[gameid].hits++;
+		}
+	}
+
+	flatten() {
+		var d = [];
+		Object.keys(this.games).forEach((key) => {
+			d.push(this.games[key]);
+		});
+		return d;
+	}
+}

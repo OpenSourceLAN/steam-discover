aef7d532b775e444edaa8c63809cec54df09c97f
Chris Holman <squid@sirsquidness.com>
2016-05-07 01:13:32 +1000

    Add UDP data sink

--- data-sink/udp.js
+++ data-sink/udp.js
@@ -1,3 +1,6 @@
+"use strict";
+
+var dgram = require('dgram');
 
 // UDP as a transport for moving events to logstash
 
@@ -6,16 +9,53 @@
 
 
 class udp {
-	constructor(opts) {
-
+	constructor(options) {
+		this.options = options;
+		this.socket = dgram.createSocket(this.options.udp6 ? 'udp6' : 'udp4');
 	}
 	insertClient(clientInfo, batchId) {
-		
+		var payload = JSON.stringify({
+			type: "client",
+			data: clientInfo
+		});
+
+		this.socket.send(payload, this.options.port, this.options.host);
 	}
 
 	insertAccount(accountInfo, batchId) {
+		// Remove some of the really long properties from the data so that we 
+		// have a payload suitable for UDP 
+		var clonedObj = cloneObjectExcept(accountInfo, [
+			"avatarFull",
+			"avatarMedium",
+			"avatar",
+			"profileUrl"
+			]);
 
+		var payload = JSON.stringify({
+			type: "account",
+			data: clonedObj
+		});
+
+		this.socket.send(payload, this.options.port, this.options.host);
 	}
 }
+
+
+function cloneObjectExcept(object, excludes) {
+	var newObj = {};
+
+	for (var i in object) {
+		if (object.hasOwnProperty(i) == false) {
+			continue;
+		}
+
+		if (excludes.indexOf(i) === -1) {
+			newObj[i] = object[i];
+		}
+	}
+	return newObj;
+}
+
 
 module.exports = udp;
